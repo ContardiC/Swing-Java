d16a4a97301462839769c5f23f8f3174d8922441
Carlo Contardi <c.contardi@spacecoding.it>
2025-02-08 15:40:04 +0100

    Initial commit

---
+++ src/main/java/it/spacecoding/Main.java
@@ -0,0 +1,11 @@
+package it.spacecoding.basic_swing_application;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello SWING");
+        frame.setVisible(true);
+    }
+
+}
