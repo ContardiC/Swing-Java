fd6b6051589a02454737c43c2e2406e77394b187
Carlo Contardi <c.contardi@spacecoding.it>
2025-02-16 17:59:35 +0100

    [Feature] add: borderTitle

--- src/main/java/it/spacecoding/swing/size/FormPanel.java
+++ src/main/java/it/spacecoding/swing/size/FormPanel.java
@@ -1,9 +1,15 @@
 package it.spacecoding.swing.size;
 
+import java.awt.Dimension;
+
 import javax.swing.*;
 
 public class FormPanel extends JPanel {
     public FormPanel(){
-
+        Dimension dim = getPreferredSize();
+        // System.out.println(dim.toString());
+        dim.width = 250;
+        setPreferredSize(dim);
+        setBorder(BorderFactory.createTitledBorder("Add Person"));
     }
 }
