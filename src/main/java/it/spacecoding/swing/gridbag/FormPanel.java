87b8e4e0a4d9ffc55a066eccce4d4a77ee16ed1d
ContardiC <c.contardi@spacecoding.it>
2025-02-17 12:01:12 +0100

    add Grid Bag

---
+++ src/main/java/it/spacecoding/swing/gridbag/FormPanel.java
@@ -0,0 +1,36 @@
+package it.spacecoding.swing.gridbag;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class FormPanel extends JPanel {
+    private JLabel nameLabel;
+    private JLabel occupationLabel;
+    private JTextField nameField;
+    private JTextField occupationField;
+    private JButton okButton;
+    public FormPanel(){
+        Dimension dim = getPreferredSize();
+        // System.out.println(dim.toString());
+        dim.width = 250;
+        setPreferredSize(dim);
+        nameLabel = new JLabel(("Name: "));
+        occupationLabel = new JLabel("Occupation: ");
+        nameField = new JTextField(10);
+        occupationField = new JTextField(10);
+
+        okButton = new JButton("Save Data");
+
+
+        Border innerBorder = BorderFactory.createTitledBorder("Add A Person");
+        Border outBorder = BorderFactory.createEmptyBorder(5,5,5,5);
+        setBorder(BorderFactory.createCompoundBorder(outBorder,innerBorder));
+        setLayout(new GridBagLayout());
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.weightx = 1 ;
+        gridBagConstraints.weighty = 1;
+    }
+}
---
+++ src/main/java/it/spacecoding/swing/gridbag/StringListener.java
@@ -0,0 +1,5 @@
+package it.spacecoding.swing.gridbag;
+
+public interface StringListener {
+    public void textEmitted(String text);
+}
