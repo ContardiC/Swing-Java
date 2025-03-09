6117d0cd47cf01a2c6bad7b37c6819e5d48509fe
Carlo Contardi <c.contardi@spacecoding.it>
2025-03-09 16:31:10 +0100

    add: Checkboxes

--- src/main/java/it/spacecoding/swing/forms/MainFrame.java
+++ src/main/java/it/spacecoding/swing/forms/MainFrame.java
@@ -17,6 +17,8 @@ public class MainFrame extends JFrame {
     private JRadioButton radioButton1;
     private JRadioButton radioButton2;
     private ButtonGroup buttonGroup;
+    private JCheckBox checkBox1;
+    private JCheckBox checkbox2;
     public MainFrame(){
         setTitle("Form");
         // TOP PANEL
@@ -30,13 +32,21 @@ public class MainFrame extends JFrame {
         add(topPanel, BorderLayout.NORTH);
         // MID PANEL
         midPanel = new JPanel();
-        radioButton1 = new JRadioButton("Opzione 1");
-        radioButton2 = new JRadioButton("Opzione 2");
+        // Radio buttons
+        radioButton1 = new JRadioButton("Scelta 1");
+        radioButton2 = new JRadioButton("Scelta 2");
         buttonGroup = new ButtonGroup();
         buttonGroup.add(radioButton1);
         buttonGroup.add(radioButton2);
+        // Checkboxes
+        checkBox1 = new JCheckBox("Opzione 1");
+        checkbox2 = new JCheckBox("Opzione 2");
+
+
         midPanel.add(radioButton1);
         midPanel.add(radioButton2);
+        midPanel.add(checkBox1);
+        midPanel.add(checkbox2);
         add(midPanel, BorderLayout.CENTER);
         // BOTTOM PANEL
         bottomPanel = new JPanel();
@@ -49,9 +59,15 @@ public class MainFrame extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 String message = "Text: ";
                 message += textField.getText();
-
-
-
+                message+=" Scelte: ";
+                if(radioButton1.isSelected()){
+                    message+=radioButton1.getText();
+                }else if(radioButton2.isSelected()){
+                    message+=radioButton2.getText();
+                }
+                message+="Opzioni: ";
+                message+= checkBox1.isSelected() ? "Opzione1 " : "";
+                message+= checkbox2.isSelected() ? "Opzione 2" : "";
 
                 dataLabel.setText(message);
             }
